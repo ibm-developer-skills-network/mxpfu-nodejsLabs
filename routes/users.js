ff2ec27213bf2cb89b1207a0526336eb15a00949
Sklup55 <85282385+Sklup55@users.noreply.github.com>
2022-09-05 15:27:22 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -22,49 +22,26 @@ let users = [
         DOB:"21-03-1989",
     },
 ];
-router.get("/:email",(req,res)=>{
-    const email = req.params.email;
-    let filtered_users = users.filter((user) => user.email === email);
-    res.send(filtered_users);
-})  
+
 router.get("/",(req,res)=>{
-    res.send(users);
+// Copy the code here
 });
+
+router.get("/:email",(req,res)=>{
+// Copy the code here
+});
+
+
 router.post("/new/",(req,res)=>{
-    users.push({"firstName":req.query.firstName,"lastName":req.query.lastName,"email":req.query.email,"DOB":req.query.DOB});
-    res.send("The user" + (' ')+ (req.query.firstName) + " Has been added!")
+// Copy the code here
 });
 
 router.put("/:email", (req, res) => {
-    const email = req.params.email;
-    let filtered_users = users.filter((user) => user.email === email);
-    if (filtered_users.length > 0) {
-        let filtered_user = filtered_users[0];
-        let DOB = req.query.DOB;
-        let firstName = req.query.firstName;
-        let lastName = req.query.lastName;
-        if(DOB) {
-            filtered_user.DOB = DOB
-        }
-        if(firstName) {
-            filtered_user.firstName = firstName
-        }
-        if(lastName) {
-            filtered_user.lastName = lastName
-        }
-        users = users.filter((user) => user.email != email);
-        users.push(filtered_user);
-        res.send(`User with the email  ${email} updated.`);
-    }
-    else{
-        res.send("Unable to find user!");
-    }
+// Copy the code here
   });
 
 router.delete("/:email", (req, res) => {
-    const email = req.params.email;
-    users = users.filter((user) => user.email != email);
-    res.send(`User with the email  ${email} deleted.`);
+// Copy the code here
   });
 
 
