9738ee99c5bee6a12fc2ffd1824b53f60bc2ab27
Sklup55 <85282385+Sklup55@users.noreply.github.com>
2022-09-07 11:51:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const express = require('express');
 
 const routes = require('./routes/users.js');
 
+// const jwt = require('jsonwebtoken');
+
 
 const app = express();
 const PORT =5000;
 
+
+app.use("/user", function (req, res, next) {
+
+    console.log('The user is logged in! Time:', Date.now())
+    next();
+});
 
 app.use("/user", routes);
 
