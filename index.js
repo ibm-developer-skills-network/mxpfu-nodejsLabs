e72a4a8d7a9f88efdbfab32a25c30098dd2ba914
Sklup55 <85282385+Sklup55@users.noreply.github.com>
2022-09-08 13:18:55 +0530

    Update index.js
    Completed authentication -- by Saptha  (8.9.22)

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
 const express = require('express');
-
 const routes = require('./routes/users.js');
-
-// const jwt = require('jsonwebtoken');
-
-
+const jwt = require('jsonwebtoken');
 const app = express();
 const PORT =5000;
-
-
-app.use("/user", function (req, res, next) {
-
-    console.log('The user is logged in! Time:', Date.now())
-    next();
+app.use(express.json());
+function auth(req,res,next){
+// Middleware which tells that the user is authenticated or not
+   let token = req.headers["authorization"];
+   token = token.split(" ")[1]; // Access Token
+   jwt.verify(token, "access",(err,user)=>{
+       if(!err){
+           req.user = user;
+           next();
+       }
+       else{
+           return res.status(403).json({message: "User not authenticated"})
+       }
 });
-
+}
+app.post("/protected", auth , (req,res)=> {
+    res.send("Access token authenticated!!");
+});
+app.post("/login", (req,res) => {
+    const user = req.body.user;
+    if (!user) {
+        return res.status(404).json({message: "Body Empty"});
+    }
+    let accessToken = jwt.sign(user ,"access", {expiresIn: '60s'});
+    let refreshToken = jwt.sign(user,"refresh", {expiresIn:'7d'});
+    return res.status(201).json({
+        accessToken,
+        refreshToken
+    })
+});
 app.use("/user", routes);
-
 app.listen(PORT,()=>console.log("Server is running"));
