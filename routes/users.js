844c464181483fecdb5c00b629758a98443e8dde
Sklup55 <85282385+Sklup55@users.noreply.github.com>
2022-09-08 13:19:20 +0530

    Update users.js
    Completed authentication -- by Saptha  (8.9.22)

--- routes/users.js
+++ routes/users.js
@@ -51,45 +51,6 @@ router.delete("/:email", (req, res) => {
 // Copy the code here
   });
 
-// JWT token generation
-// app.post("/user/generateToken", (req, res) => {
-//     // Validate User Here
-//     // Then generate JWT Token
-  
-//     let jwtSecretKey = process.env.JWT_SECRET_KEY;
-//     let data = {
-//         time: Date(),
-//         userId: 12,
-//     }
-  
-//     const token = jwt.sign(data, jwtSecretKey);
-  
-//     res.send(token);
-// });  
-
-// Generated JWT token verification
-// app.get("/user/validateToken", (req, res) => {
-//     // Tokens are generally passed in the header of the request
-//     // Due to security reasons.
-  
-//     let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
-//     let jwtSecretKey = process.env.JWT_SECRET_KEY;
-  
-//     try {
-//         const token = req.header(tokenHeaderKey);
-  
-//         const verified = jwt.verify(token, jwtSecretKey);
-//         if(verified){
-//             return res.send("Successfully Verified");
-//         }else{
-//             // Access Denied
-//             return res.status(401).send(error);
-//         }
-//     } catch (error) {
-//         // Access Denied
-//         return res.status(401).send(error);
-//     }
-// });
 
 
 
