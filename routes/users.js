36c97ff17fa3f6f546b090cadc42379177100109
lavskillup <lavanyas@skillup.tech>
2022-09-13 11:28:08 +0530

    added session logic

--- routes/users.js
+++ routes/users.js
@@ -25,33 +25,37 @@ let users = [
 
 // GET request: Retrieve all users
 router.get("/",(req,res)=>{
-// Copy the code here
+
+  // Copy the code here
+
+  res.send("Yet to be implemented")//This line is to be replaced with actual return value
 });
 
 // GET by specific ID request: Retrieve a single user with email ID
 router.get("/:email",(req,res)=>{
-// Copy the code here
+  // Copy the code here
+  res.send("Yet to be implemented")//This line is to be replaced with actual return value
 });
 
 
 // POST request: Create a new user
 router.post("/new/",(req,res)=>{
-// Copy the code here
+  // Copy the code here
+  res.send("Yet to be implemented")//This line is to be replaced with actual return value
 });
 
 
 // PUT request: Update the details of a user by email ID
 router.put("/:email", (req, res) => {
-// Copy the code here
-  });
+  // Copy the code here
+  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+});
 
 
 // DELETE request: Delete a user by email ID
 router.delete("/:email", (req, res) => {
-// Copy the code here
-  });
-
-
-
+  // Copy the code here
+  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+});
 
 module.exports=router;
