561ee0e8a7417fb6ef965562d1a2a085d1e2fc2f
lavskillup <73978832+lavskillup@users.noreply.github.com>
2022-11-17 13:05:44 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -37,7 +37,7 @@ router.get("/:email",(req,res)=>{
 
 
 // POST request: Create a new user
-router.post("/new/",(req,res)=>{
+router.post("/",(req,res)=>{
   // Copy the code here
   res.send("Yet to be implemented")//This line is to be replaced with actual return value
 });
