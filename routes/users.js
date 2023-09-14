167331d84d13b9343e2fc1d6095015622173e9ce
Jack <143157408+JackSavino@users.noreply.github.com>
2023-09-14 15:32:38 -0600

    Update users.js
    Completing update of coding from the lab

--- routes/users.js
+++ routes/users.js
@@ -25,14 +25,37 @@ let users = [
 
 // GET request: Retrieve all users
 router.get("/",(req,res)=>{
-    res.send(users);
+    res.send(JSON.stringify({users},null,4));
 });
 
 // GET by specific ID request: Retrieve a single user with email ID
 router.get("/:email",(req,res)=>{
-  const email = req.params.email;
-  let filtered_users = users.filter((user) => user.email === email);
-  res.send(filtered_users);
+    const email = req.params.email;
+    let filtered_users = users.filter((user) => user.email === email);
+    res.send(filtered_users);
+});
+
+// Sort users by DOB
+function getDateFromString(strDate) {
+    let [dd,mm,yyyy] = strDate.split('-')
+    return new Date(yyyy+"/"+mm+"/"+dd);
+}
+    
+// console.log(sorted_users);
+router.get("/sort",(req,res)=>{
+    let sorted_users=users.sort(function(a, b) {
+        let d1 = getDateFromString(a.DOB);
+        let d2 = getDateFromString(b.DOB);
+            return d1-d2;
+          });
+    res.send(sorted_users);
+});
+
+// GET users with a particular Last Name eg. 'Smith'
+router.get("/lastName/:lastName",(req,res)=>{
+    const lastName = req.params.lastName;
+    let filtered_lastname = users.filter((user) => user.lastName === lastName);
+    res.send(filtered_lastname);
 });
 
 
