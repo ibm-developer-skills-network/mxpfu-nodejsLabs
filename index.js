9af3f68461339461f24d236749a7ae9863a2a3bc
Jugador7 <moyagabriel14@gmail.com>
2024-05-09 14:58:43 -0500

    finished hands-on boss

--- index.js
+++ index.js
@@ -4,8 +4,32 @@ const routes = require('./routes/users.js');
 const app = express();
 const PORT =5000;
 
-app.use(express.json());
+app.use(express.json()); //Observe that the express app uses the middleware express.json() to handle the request as a json object.
 
-app.use("/user", routes);
+app.use("/user", routes); //Observe that the express app uses routes to handle the endpoints which start with /user. This means that for all the endpoints starting with /user, the server will go and look for an endpoint handler in users.js.
+
+app.use("/sort", routes);
 
 app.listen(PORT,()=>console.log("Server is running at port "+PORT));
+
+
+// Recall that GET, POST, PUT and DELETE are the commonly used HTTP methods to perform CRUD operations. Those operations retrieve and send data to the server.
+
+// GET is used to request data from a specified resource.
+
+// POST is used to send data to a server for creating a resource.
+
+// PUT is used to send data to a server to update a resource.
+
+// DELETE is used for deleting a specified resource.
+
+// POST AND PUT are sometimes used interchangeably.
+
+// This lab requires some packages to be installed. The express and nodemon package for starting and running the Express server and jsonwebtoken and express-session for session based authentication.
+// express - This is for creating a server to serve the API endpoints.
+// nodemon - This will help to restart the server when you make any changes to the code.
+// jsonwebtoken - This package helps in generating a JSON web token which we will use for authentication. A JSON web token (JWT) is a JSON object used to communicate information securely over the internet (between two parties). It can be used for information exchange and is typically used for authentication systems.
+// express-session - This package will help us to maintain the authentication for the session.
+//These packages are defined in as dependencies in packages.json.
+
+//Observe that the express app uses the middleware express.json() to handle the request as a json object.
--- index_withauth.js
+++ index_withauth.js
@@ -8,10 +8,22 @@ const PORT =5000;
 
 app.use(session({secret:"fingerpint",resave: true, saveUninitialized: true}))
 
+//This tells your express app to use the session middleware.
+// secret - a random unique string key used to authenticate a session.
+// resave - takes a Boolean value. It enables the session to be stored back to the session store, 
+// even if the session was never modified during the request. 
+// saveUninitialized - this allows any uninitialized session to be sent to the store. 
+// When a session is created but not modified, it is referred to as uninitialized.
+
 app.use(express.json());
 
 app.use("/user", (req,res,next)=>{
 // Middleware which tells that the user is authenticated or not
+//Observe the implementation of the authentication middleware. 
+//All the endpoints starting with /user will go through this middleware. 
+//It will retrieve the authorization details from the session and verify it. 
+//If the token is validated, the user is authenticated and the control is passed on to the next endpoint handler. 
+//If the token is invalid, the user is not authenticated and an error message is returned.
    
    if(req.session.authorization) {
        let token = req.session.authorization['accessToken']; // Access Token
@@ -46,5 +58,12 @@ app.post("/login", (req,res) => {
     }
     return res.status(200).send("User successfully logged in");
 });
+
+// Observe the implementation of the login endpoint. 
+//A user logs into the system providing a username. 
+//An access token that is valid for one hour is generated. 
+//You may observe this validty length specified by 60 * 60, which signifies the time in seconds. 
+//This access token is set into the session object to ensure that only authenticated users can access the endpoints 
+//for that length of time.
 
 app.listen(PORT,()=>console.log("Server is running at port "+PORT));
