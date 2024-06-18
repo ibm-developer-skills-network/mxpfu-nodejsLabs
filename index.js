6364ca439a1f692dbf90eaec057c92459b39616d
Sklup55 <85282385+Sklup55@users.noreply.github.com>
2024-06-18 21:57:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+// Import the Express library
 const express = require('express');
+// Import the routes from the users.js file
 const routes = require('./routes/users.js');
 
+// Create an instance of an Express application
 const app = express();
-const PORT =5000;
+// Define the port number the server will listen on
+const PORT = 5000;
 
+// Use the express.json middleware to parse JSON request bodies
 app.use(express.json());
 
+// Use the imported routes for handling requests to paths that start with "/user"
 app.use("/user", routes);
 
-app.listen(PORT,()=>console.log("Server is running at port "+PORT));
+// Start the server and listen on the specified port, logging a message when the server is running
+app.listen(PORT, () => console.log("Server is running at port " + PORT));
